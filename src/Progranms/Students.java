71ebfd6160c09aaff118046cdc473f560271cc96
home1 <73322461+HZYcms@users.noreply.github.com>
2020-11-14 17:09:10 +0800

    Update Students.java

--- src/Progranms/Students.java
+++ src/Progranms/Students.java
@@ -1,12 +1,13 @@
 package Progranms;
 //定义一个学生类
 class Students {
-    public String name;
-    public float mathsocer;
-    public float Englishsocer;
-    public float linearscoer;
-    public float gymsocer;
-    public float all;
+
+    private String name;
+    private float mathsocer;
+    private float Englishsocer;
+    private float linearscoer;
+    private float gymsocer;
+    private float all;
 
     public Students() {
     }
@@ -30,27 +31,44 @@ class Students {
         this.name = name;
     }
 
+    public float getMathsocer() {
+        return mathsocer;
+    }
 
     public void setMathsocer(float mathsocer) {
         this.mathsocer = mathsocer;
     }
 
+    public float getEnglishsocer() {
+        return Englishsocer;
+    }
 
     public void setEnglishsocer(float englishsocer) {
         Englishsocer = englishsocer;
     }
 
+    public float getLinearscoer() {
+        return linearscoer;
+    }
 
     public void setLinearscoer(float linearscoer) {
         this.linearscoer = linearscoer;
     }
 
+    public float getGymsocer() {
+        return gymsocer;
+    }
 
     public void setGymsocer(float gymsocer) {
         this.gymsocer = gymsocer;
     }
 
+    public float getAll() {
+        return all;
+    }
+
     public void setAll(float all){
         this.all=all;
     }
+
 }
